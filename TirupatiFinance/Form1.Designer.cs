17934addddad9e297c03257ed0f846fc76448449
prafulla <prafullakokadwar@gmail.com>
2022-08-07 22:46:47 +0530

    testing

--- TirupatiFinance/Form1.Designer.cs
+++ TirupatiFinance/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1243, 666);
+            this.ClientSize = new System.Drawing.Size(1006, 551);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
